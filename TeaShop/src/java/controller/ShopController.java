6b2c1ddcd63fd19227dc68a0394725e9e1c3c10c
HoangDev2003 <hoangnxhe171895@fpt.edu.vn>
2024-05-24 08:05:53 +0700

    init project

---
+++ TeaShop/src/java/controller/ShopController.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+package controller;
+
+import dal.implement.CategoryDAO;
+import dal.implement.ProductDAO;
+import entity.Category;
+import entity.Product;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.util.List;
+
+/**
+ *
+ * @author HoangNX
+ */
+public class ShopController extends HttpServlet {
+   
+    
+    
+
+    
+}
