72e988a5c890ac64f3462b09028adfde98e101d8
phamtoan198 <toanpkhe173035@fpt.edu.vn>
2024-06-28 09:03:28 +0700

    Insert Category Controller

--- TeaShop2/src/java/controller/CategoryManagerController.java
+++ TeaShop2/src/java/controller/CategoryManagerController.java
@@ -22,33 +22,32 @@ public class CategoryManagerController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String service = req.getParameter("Service");
-        
-        if(service == null){
+
+        if (service == null) {
             service = "listAllCategory";
         }
-        
-        if(service.equals("listAllCategory")){
+
+        if (service.equals("listAllCategory")) {
             List<Category> listCategory = (new CategoryDAO().findAll());
             req.setAttribute("listAllcategory", listCategory);
             req.getRequestDispatcher("view/dashboard/admin/categoryManagement.jsp").forward(req, resp);
         }
-        
-//        if(service.equals("insertCategory")){
-//            List<Category> listCategory = (new CategoryDAO().findAll());
-//            req.setAttribute("listAllcategory", listCategory);
-//            req.getRequestDispatcher("view/dashboard/admin/categoryManagement.jsp").forward(req, resp);
-//        }
-//        
-//        if(service.equals("sendRequestInsert")){
-//            String name = req.getParameter("name");
-//            
-//            Category category = new Category(name);
-//            int generatedCategoryId = (new CategoryDAO().insertCategory(category));
-//            req.setAttribute("InsertDone","Insert Category (ID =" + generatedCategoryId + ") successfully!\n click Category Management to see all changes");
-//            req.getRequestDispatcher("view/dashboard/admin/categoryManagement.jsp").forward(req, resp);
-//        }
-        
+
+        if (service.equals("insertCategory")) {
+            List<Category> listCategory = (new CategoryDAO().findAll());
+            req.setAttribute("listAllcategory", listCategory);
+            req.getRequestDispatcher("view/dashboard/admin/categoryManagement.jsp").forward(req, resp);
+        }
+
+        if (service.equals("sendRequestInsert")) {
+            String name = req.getParameter("name");
+
+            Category category = new Category(name);
+            int generatedCategoryId = (new CategoryDAO().insertCategory(category));
+            req.setAttribute("InsertDone", "Insert Category (ID =" + generatedCategoryId + ") successfully!\n click Category Management to see all changes");
+            req.getRequestDispatcher("view/dashboard/admin/categoryManagement.jsp").forward(req, resp);
+        }
+
     }
-    
 
 }
