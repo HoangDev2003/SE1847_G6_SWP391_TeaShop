408fb102ad1aa0d6e715ec3def5e5a141e5beaf3
huyentrangne <tranghthhe171368@fpt.edu.vn>
2024-07-12 03:08:59 +0700

    Update AddShipper

--- TeaShop2/src/java/controller/AddUserController.java
+++ TeaShop2/src/java/controller/AddUserController.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package controller;
 
 import dal.AccountDAO;
@@ -25,34 +24,37 @@ import java.util.logging.Level;
  * @author Huyen Tranq
  */
 public class AddUserController extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet AddUserController</title>");  
+            out.println("<title>Servlet AddUserController</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet AddUserController at " + request.getContextPath () + "</h1>");
+            out.println("<h1>Servlet AddUserController at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
-    } 
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -60,12 +62,13 @@ public class AddUserController extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         request.getRequestDispatcher("AddUser.jsp").forward(request, response);
-    } 
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -73,7 +76,7 @@ public class AddUserController extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         AdminDAO dao = new AdminDAO();
         AccountDAO da = new AccountDAO();
         String name = request.getParameter("name");
@@ -84,9 +87,9 @@ public class AddUserController extends HttpServlet {
         String address = request.getParameter("address");
         String role = request.getParameter("role");
         String status = request.getParameter("status");
-       
+
         int role_id = Integer.parseInt(role);
-        int status_id = Integer.parseInt(status); 
+        int status_id = Integer.parseInt(status);
         System.out.println(name);
         System.out.println(email);
         System.out.println(pass);
@@ -94,12 +97,15 @@ public class AddUserController extends HttpServlet {
         System.out.println(gender);
         System.out.println(address);
         System.out.println(role);
-        System.out.println(status);     
+        System.out.println(status);
         Accounts a = da.checkAccountExist(email);
         if (a == null) {
             if (role_id == 3) {
                 dao.addUser(email, pass, name, role_id, status_id, phone, gender, address);
                 response.sendRedirect("staffmanager");
+            } else if (role_id == 4) {
+                dao.addUser(email, pass, name, role_id, status_id, phone, gender, address);
+                response.sendRedirect("shippermanager");
             } else {
                 dao.addUser(email, pass, name, role_id, status_id, phone, gender, address);
                 response.sendRedirect("customerManagement");
@@ -110,8 +116,9 @@ public class AddUserController extends HttpServlet {
         }
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
